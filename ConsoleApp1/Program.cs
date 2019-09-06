79f4678d57eb0988f8101ece6c92f8b3def162f2
Andri Suga <grazy.frog@hotmail.com>
2019-09-06 18:10:45 +0300

    m

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SatNam");
+            Console.WriteLine("Mis on sinu nimi?");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("SatNam, " + name);
+            Console.ReadLine();
+
+
+        }
+    }
+}
